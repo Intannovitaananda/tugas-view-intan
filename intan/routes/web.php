8064f357c2c048fd6e23874a4664f83bac08e6ed
intannovitaananda <intannovitaanada@gmail.com>
2024-05-08 11:51:26 +0700

    intan

---
+++ intan/routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('form', function () {
+    return view('form');
+});
+
+Route::get('welcome', function () {
+    return view('welcome');
+});
+
+
